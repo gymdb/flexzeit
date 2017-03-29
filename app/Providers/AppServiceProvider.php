c90370e783309c8bd35d99818fec99e91519a419
Daniel Rohr <rohr.daniel@gmail.com>
2017-03-30 01:35:29 +0200

    AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,41 @@
 
 namespace App\Providers;
 
+use App\Repositories\ConfigRepository;
+use App\Repositories\GroupRepository;
+use App\Repositories\LessonRepository;
+use App\Repositories\OffdayRepository;
+use App\Services\ConfigService;
+use App\Services\CourseService;
+use App\Services\LessonService;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class AppServiceProvider extends ServiceProvider {
 
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot() {
+  }
+
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register() {
+    // Bind repositories
+    $this->app->bind(ConfigRepository::class, \App\Repositories\Eloquent\ConfigRepository::class);
+    $this->app->bind(GroupRepository::class, \App\Repositories\Eloquent\GroupRepository::class);
+    $this->app->bind(LessonRepository::class, \App\Repositories\Eloquent\LessonRepository::class);
+    $this->app->bind(OffdayRepository::class, \App\Repositories\Eloquent\OffdayRepository::class);
+
+    // Services
+    $this->app->bind(ConfigService::class, \App\Services\Implementation\ConfigService::class);
+    $this->app->bind(CourseService::class, \App\Services\Implementation\CourseService::class);
+    $this->app->bind(LessonService::class, \App\Services\Implementation\LessonService::class);
+  }
+
 }
