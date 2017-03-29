7c0383c167aeb844dcc520c198513b2ac3e0b292
Daniel Rohr <rohr.daniel@gmail.com>
2017-03-30 01:27:01 +0200

    Config files

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/login', 'Auth\LoginController@showLoginForm');
+Route::post('/login', 'Auth\LoginController@login')->name('loginTarget');
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/student', 'HomeController@index')->name('student');
+Route::get('/teacher', 'HomeController@index')->name('teacher');
