8c5020b7de5dac976656cdad74b00ef4432d4661
Daniel Rohr <rohr.daniel@gmail.com>
2017-07-25 12:36:48 +0200

    Command scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Services\ConfigService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,6 +25,19 @@ class Kernel extends ConsoleKernel {
    * @return void
    */
   protected function schedule(Schedule $schedule) {
+    $configService = $this->app->make(ConfigService::class);
+
+    $lessons = $configService->getLessonTimes();
+    foreach ($lessons as $d => $times) {
+      foreach ($times as $n => $time) {
+        $schedule->command('untis:absences')
+            ->days($d)
+            ->at($time['start']);
+      }
+    }
+
+    $schedule->command('untis:offdays')
+        ->twiceDaily(6, 18);
   }
 
   /**
