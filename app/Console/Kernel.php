72e554b6090113c6f029e0b0ef20a76af5a0d445
Daniel Rohr <rohr.daniel@gmail.com>
2017-07-25 13:38:02 +0200

    Fix in console scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,7 +4,9 @@ namespace App\Console;
 
 use App\Services\ConfigService;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel {
 
@@ -25,15 +27,20 @@ class Kernel extends ConsoleKernel {
    * @return void
    */
   protected function schedule(Schedule $schedule) {
-    $configService = $this->app->make(ConfigService::class);
+    try {
+      $configService = $this->app->make(ConfigService::class);
 
-    $lessons = $configService->getLessonTimes();
-    foreach ($lessons as $d => $times) {
-      foreach ($times as $n => $time) {
-        $schedule->command('untis:absences')
-            ->days($d)
-            ->at($time['start']);
+      $lessons = $configService->getLessonTimes();
+      foreach ($lessons as $d => $times) {
+        foreach ($times as $n => $time) {
+          $schedule->command('untis:absences')
+              ->days($d)
+              ->at($time['start']);
+        }
       }
+    } catch (QueryException $e) {
+      // This can happen on deployment
+      Log::error('Failed scheduling absences loading.', [$e->getMessage()]);
     }
 
     $schedule->command('untis:offdays')
