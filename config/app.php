f507a3e982862ff3f8ce3292f5f3f13402c19522
Daniel Rohr <rohr.daniel@gmail.com>
2017-09-21 16:12:55 +0200

    Remove manual package registration which is not necessary anymore

--- config/app.php
+++ config/app.php
@@ -164,11 +164,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
       /*
-       * Package Service Providers...
-       */
-        HTMLMin\HTMLMin\HTMLMinServiceProvider::class,
-
-      /*
        * Application Service Providers...
        */
         App\Providers\AppServiceProvider::class,
